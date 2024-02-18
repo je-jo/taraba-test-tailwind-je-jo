6ffa119ddefa98cbcc1a0f39dac1f74d9eb65212
Jelena Jovicic <jejovicic@gmail.com>
2024-02-18 19:29:52 +0100

    features section

--- src/js/validation.js
+++ src/js/validation.js
@@ -6,8 +6,9 @@ const termsCheck = document.querySelector("#terms");
 form.addEventListener("submit", (e) => {
     
     if (termsCheck.validity.valueMissing) {
-        termsCheck.parentElement.style.color = "#FF1934"
-        termsCheck.style.borderColor = "#FF1934"
+        termsCheck.parentElement.style.color = "#FF1934";
+        termsCheck.style.borderColor = "#FF1934";
         e.preventDefault();
+
     }
 })
